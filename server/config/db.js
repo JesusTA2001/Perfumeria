a497656be9c80237843b976ee67a4ceb095184d4
Jesus Torres Alvarez <msc_jtorres2024@accitesz.com>
2026-07-09 16:41:36 -0600

    modificar ssl para usar base de datos de aiven

--- server/config/db.js
+++ server/config/db.js
@@ -1,11 +1,31 @@
 const mysql = require('mysql2/promise');
 
+function buildSslConfig() {
+  const sslMode = (process.env.DB_SSL_MODE || '').toUpperCase();
+  const useSsl = process.env.DB_SSL === 'true' || sslMode === 'REQUIRED';
+
+  if (!useSsl) {
+    return undefined;
+  }
+
+  const sslConfig = {
+    rejectUnauthorized: process.env.DB_SSL_REJECT_UNAUTHORIZED !== 'false',
+  };
+
+  if (process.env.DB_SSL_CA) {
+    sslConfig.ca = process.env.DB_SSL_CA.replace(/\\n/g, '\n');
+  }
+
+  return sslConfig;
+}
+
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: buildSslConfig(),
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const { app, initDbPromise } = require('./app');
+const pool = require('./config/db');
 
 const PORT = Number(process.env.API_PORT || 4000);
 
@@ -7,6 +8,18 @@ initDbPromise.then(() => {
     // eslint-disable-next-line no-console
     console.log(`API running on http://localhost:${PORT}`);
   });
+
+  // Keep-alive ping a la base de datos cada 10 minutos
+  setInterval(async () => {
+    try {
+      await pool.query('SELECT 1');
+      // eslint-disable-next-line no-console
+      console.log('Database keep-alive ping: success');
+    } catch (err) {
+      // eslint-disable-next-line no-console
+      console.error('Database keep-alive ping failed:', err.message);
+    }
+  }, 10 * 60 * 1000);
 });
 
 
